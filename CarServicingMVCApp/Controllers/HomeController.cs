6edd5e27f2134df1f79279417147fa37eb2ff7fe
ankit88d <100204996+ankit88d@users.noreply.github.com>
2022-02-22 18:38:29 +0530

    Add files via upload

---
+++ CarServicingMVCApp/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using CarServicing.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarServicing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<CarViewModel> carlist;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:58738/");
+                var responseTask = client.GetAsync("CarService");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<CarViewModel>>();
+                    readTask.Wait();
+                    carlist = readTask.Result;
+                }
+                else
+                {
+                    carlist = Enumerable.Empty<CarViewModel>();
+                }
+            }
+
+            return View(carlist);
+        }
+     
+
+            [HttpPost]     
+        public ActionResult CarView(CarViewModel car)
+        {          
+            IEnumerable<CarViewModel> carlist;
+            using (var client = new HttpClient())
+            {                
+                client.BaseAddress = new Uri("http://localhost:58738/");
+                var response =  client.PostAsync("CarService", car, new JsonMediaTypeFormatter());
+
+                response.Wait();
+
+                var result = response.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<CarViewModel>>();
+                    readTask.Wait();
+                    carlist = readTask.Result;
+                }
+                else
+                {
+                    carlist = Enumerable.Empty<CarViewModel>();
+                }
+            }
+            return View("Index", carlist);
+
+        }
+        
+        public ActionResult CarView()
+        {
+            return View();
+
+        }
+
+
+    }
+}
---
+++ CarServicingMVCApp/Models/CarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarServicing.Models
+{
+    public class CarViewModel
+    {
+        public string RegistrationNumber { get; set; }
+        public string Owner { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Status { get; set; }
+        public string Service { get; set; }
+
+    }
+}
